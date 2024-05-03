baa4bf7fc1dfb6a5fe6c0b569abc93966575ce9a
rolemberg <vinnie.nsts@gmail.com>
2024-05-03 01:51:12 -0300

    created Product class;

--- src/com/santander/interfaceset/Main.java
+++ src/com/santander/interfaceset/Main.java
@@ -1,23 +1,39 @@
 package com.santander.interfaceset;
 
 import com.santander.interfaceset.OpBasic.GuestsCollection;
+import com.santander.interfaceset.searchInSet.Contact;
+import com.santander.interfaceset.searchInSet.Phonebook;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            GuestsCollection guestsCollection = new GuestsCollection();
-            guestsCollection.addGuest("Logan", 1123);
-            guestsCollection.addGuest("Xandy", 4468);
-            guestsCollection.addGuest("Melina", 9000);
-            guestsCollection.addGuest("Ally", 4466);
-            guestsCollection.addGuest("Ally", 4466);
+        //    GuestsCollection guestsCollection = new GuestsCollection();
+        //    guestsCollection.addGuest("Logan", 1123);
+        //    guestsCollection.addGuest("Xandy", 4468);
+        //    guestsCollection.addGuest("Melina", 9000);
+        //    guestsCollection.addGuest("Ally", 4466);
+        //    guestsCollection.addGuest("Ally", 4466);
 
-            System.out.println(guestsCollection.countGuests());
-            guestsCollection.showAll();
-            System.out.println("\n--- removed logan ---");
-            guestsCollection.removeGuestById(1123);
-            guestsCollection.countGuests();
-            guestsCollection.showAll();
+        //    System.out.println(guestsCollection.countGuests());
+        //    guestsCollection.showAll();
+        //    System.out.println("\n--- removed logan ---");
+        //    guestsCollection.removeGuestById(1123);
+        //    guestsCollection.countGuests();
+        //    guestsCollection.showAll();
+
+            Phonebook phonebook = new Phonebook();
+            phonebook.addContact("Jasml", 119987554);
+            phonebook.addContact("Jasml santos", 119987554);
+            phonebook.addContact("Jasml gabriel", 1144689232);
+            phonebook.addContact("Jasml nogueira", 113345674);
+            phonebook.addContact("Jasml santana", 1199890090);
+
+            phonebook.showAll();
+
+            for(Contact c: phonebook.searchByName("Jasml")) System.out.println("\n"+c);
+
+            phonebook.updateNumber("Jasml santana", 1177426778);
+            phonebook.showAll();
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
---
+++ src/com/santander/interfaceset/Ordenation/Store/Product.java
@@ -0,0 +1,56 @@
+package com.santander.interfaceset.Ordenation.Store;
+
+import java.util.Objects;
+
+public class Product {
+
+    private String name;
+    private int id;
+    private int quantity;
+    private double price;
+
+    public Product(String name, int id, int quantity, double price) {
+        this.name = name;
+        this.id = id;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return(
+            "Product{" + "name='" + name + '\'' +
+            ", id=" + id + ", quantity=" + quantity +
+            ", price=" + price + '}'
+        );
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
