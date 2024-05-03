55b0fe2974f7695172fd0d4f2ee1e7554e36e6ae
rolemberg <vinnie.nsts@gmail.com>
2024-05-03 01:09:53 -0300

    created GuestCollection overrided methods for hash, and testing on Main;

--- src/com/santander/interfaceset/Main.java
+++ src/com/santander/interfaceset/Main.java
@@ -1,8 +1,25 @@
 package com.santander.interfaceset;
 
+import com.santander.interfaceset.OpBasic.GuestsCollection;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            GuestsCollection guestsCollection = new GuestsCollection();
+            guestsCollection.addGuest("Logan", 1123);
+            guestsCollection.addGuest("Xandy", 4468);
+            guestsCollection.addGuest("Melina", 9000);
+            guestsCollection.addGuest("Ally", 4466);
 
+            System.out.println(guestsCollection.countGuests());
+            guestsCollection.showAll();
+            System.out.println("\n--- removed logan ---");
+            guestsCollection.removeGuestById(1123);
+            guestsCollection.countGuests();
+            guestsCollection.showAll();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
 
--- src/com/santander/interfaceset/OpBasic/GuestsCollection.java
+++ src/com/santander/interfaceset/OpBasic/GuestsCollection.java
@@ -1,6 +1,7 @@
 package com.santander.interfaceset.OpBasic;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class GuestsCollection {
@@ -8,10 +9,6 @@ public class GuestsCollection {
     private final Set<Guest> guestSet = new HashSet<>();
 
     public GuestsCollection() {}
-
-    public Set<Guest> getGuestSet() {
-        return guestSet;
-    }
 
     public void addGuest(String name, int id) {
         this.guestSet.add(new Guest(name, id));
@@ -32,6 +29,19 @@ public class GuestsCollection {
     public void showAll() throws RuntimeException {
         if(this.guestSet.isEmpty()) throw new RuntimeException("Guests list empty.");
         for(Guest g: this.guestSet) System.out.println(g);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GuestsCollection that = (GuestsCollection) o;
+        return Objects.equals(guestSet, that.guestSet);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(guestSet);
     }
 }
 
